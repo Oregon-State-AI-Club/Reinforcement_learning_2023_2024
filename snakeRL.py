61f18f14e9dd7b24a8bf09646b59e19e1808d1ee
San Davran <davrans@oregonstate.edu>
2024-02-09 18:37:51 -0800

    Create the Agent template, and the description of the main method of it.

--- snakeRL.py
+++ snakeRL.py
@@ -168,6 +168,48 @@ class SnakeGame:
         """
         pass 
 
+def Agent: 
+    def __init__( self ) : 
+        """
+        1. Initialize the agent. 
+        """
+        pass 
+
+    def learn_and_then_evaluate( self , train_count , test_interval , trial_count , create_visual_at_train = False , create_visual_at_test = False ) : 
+        """ 
+        1. Initialize a return list. 
+        2. Call learn up to train_count time. 
+        3. Each test_interval count call evaluate. 
+            2.a. Store the result from the evaluate to the return list. 
+        4. Return the return list. 
+        """ 
+        pass 
+
+    def learn( self , create_visual = False ) : 
+        """ 
+        1. Implement the RL algorithm. 
+        """ 
+        pass 
+
+    def evaluate( self , trial_count , create_visual = False ) : 
+        """ 
+        1. Test the current model up to trial_count time. 
+        2. Return the average result. 
+        """ 
+        pass 
+
+########################## 
+# 
+# How To Run It: 
+# agent = Agent() 
+# result = agent.learn_and_then_evaluate() 
+# 
+# At this time, the result is how good that the agent did. 
+# We can test it, using just the evaluate. 
+# 
+##########################
+
+
 if __name__=='__main__':
     game = SnakeGame()
 
